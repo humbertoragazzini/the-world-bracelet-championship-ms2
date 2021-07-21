0244327b51e5a50d6d5646acf7ebc5e6dc3fa5cd
humbertoragazzini <beto_vicio@hotmail.com>
2021-07-21 08:08:18 +0000

    added start style, starting main menu

--- assets/js/main-effects.js
+++ assets/js/main-effects.js
@@ -1,30 +1,3 @@
-
-
-
-/*
-function moving(){
-  var speed = Math.round(Math.random()) * 15;
-  var theinterval = setInterval(movingVertical,1000,speed);
-  setTimeout(clearInterval,3000,theinterval);
-}
-
-function interval(){
-  setInterval(moving,3000);
-}
-
-
-function movingVertical(speed){
-  if(Number(document.getElementById('circle').style.top.replace( /px/, ''))<document.getElementById("thebody").getBoundingClientRect().height){
-  document.getElementById('circle').style.top = String(Number(document.getElementById('circle').style.top.replace( /px/, ''))+speed)+"px";
-  console.log("velocidad "+speed);
-  }else{
-    document.getElementById('circle').style.top = "0px";
-  }
-}
-*/
-
-
-
 function animationJewels(){
 
   var movements = [];
@@ -43,16 +16,20 @@ function animationJewels(){
 function moving(jewel,speed){
 
   if((jewel.style.top.replace( /px/, '')) < document.getElementById("thebody").getBoundingClientRect().height)
-  { 
-   jewel.style.top = String(Number(jewel.style.top.replace( /px/, ''))+speed)+"px";
+    { 
+      jewel.style.top = String(Number(jewel.style.top.replace( /px/, ''))+speed)+"px";
     
-  }else{
-    jewel.style.top = "-50px";
-    jewel.style.left = String((Math.round(Math.random() * (document.getElementById("animated-background").getBoundingClientRect().right - document.getElementById("animated-background").getBoundingClientRect().left)))) + "px";
+    }else{
+      jewel.style.top = "-50px";
+      jewel.style.left = String((Math.round(Math.random() * (document.getElementById("animated-background").getBoundingClientRect().right - document.getElementById("animated-background").getBoundingClientRect().left)))) + "px";
+    }
+
   }
 
 }
 
-
-
+function jewelsCreation(){
+  body = document.getElementById('thebody');
+  
 }
+
