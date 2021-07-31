c5768858c0cd5cdec81e581bc43ec54f06074fb9
humbertoragazzini <beto_vicio@hotmail.com>
2021-07-31 17:12:33 +0000

    starting with player class and function to get and set data to local storage

---
+++ assets/js/game.js
@@ -0,0 +1,95 @@
+class Player{
+
+    constructor(name,record,level,identifier){
+        this.name=name;
+        this.record=record;
+        this.level=level;
+        this.identifier=identifier;
+    }
+  
+    nameAsig(name){
+        this.name=name;
+    }
+    recordAsig(record){
+        this.record=record;
+    }
+    levelAsig(level){
+        this.level=level;
+    }
+    identifierAsig(identifier){
+        this.identifier=identifier;
+    }
+  
+  }
+
+  function recoverUser(data){
+      let user = new Player(name,record,level,identifier);
+  }
+
+  var beto = new Player("jorge",156488,4,1);
+
+
+  localStorage.setItem('1',beto.name+'/'+beto.record+'/'+beto.level+'/'+beto.identifier);
+
+  function recoverDataFromLocal(user,key){
+      let name = '';
+      let record = '';
+      let level = '';
+      let cache = '';
+      let identifier = key;
+      let i = 0;
+      lengthofdata = user.length;
+
+    while(i<lengthofdata){
+        if(user[0]!='/'){
+          name=name+user[0];
+          cache = user.substring(1);
+          user = cache;
+          i++;
+        }else{
+            cache = user.substring(1);
+            user = cache;
+            lengthofdata = cache.length;
+            i = 0;
+            break;
+        }
+    }
+    
+    while(i<lengthofdata){
+        if(user[0]!='/'){
+          record=record+user[0];
+          cache = user.substring(1);
+          user = cache;
+          i++;
+        }else{
+            cache = user.substring(1);
+            user = cache;
+            lengthofdata = cache.length;
+            i = 0;
+            break;
+        }
+    }
+    while(i<lengthofdata){
+        if(user[0]!='/'){
+          level=level+user[0];
+          cache = user.substring(1);
+          user = cache;
+          i++;
+        }else{
+            cache = user.substring(1);
+            user = cache;
+            lengthofdata = cache.length;
+            i=0;
+            break;
+        }
+    }
+
+    
+      
+    let usuario = new Player(name,record,level,identifier);
+
+    return usuario;
+  }
+
+  let numero = '1';
+  console.log(recoverDataFromLocal(localStorage.getItem(numero),numero));
