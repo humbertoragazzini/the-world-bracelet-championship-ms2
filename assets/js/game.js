502e41f2b19cae558897c3f12f4c45834030034b
humbertoragazzini <beto_vicio@hotmail.com>
2021-08-04 08:24:26 +0000

    added first color button

--- assets/js/game.js
+++ assets/js/game.js
@@ -104,7 +104,14 @@ function startGame(){
     }
 }
 
-
+document.addEventListener("DOMContentLoaded",function(){
+new jBox('Modal', {
+    width: 300,
+    height: 100,
+    attach: '#myModal',
+    title: 'My Modal Window',
+    content: '<i>Hello there!</i>'
+  })})
 
 
 
